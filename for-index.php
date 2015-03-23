f4f877bbbd7332855dfdb1f7fb08d03148cbbe6e
Francis Lewis <francislewis@users.noreply.github.com>
2015-03-23 20:50:29 +0000

    Create index.php

---
+++ for-index.php
@@ -0,0 +1,7 @@
+#How-to
+Quite a few times I have found myself searching the internet tirelessly trying to find an answer to my questions.
+So, I made this site to try and help others find solutions to the answers I couldn't find.
+
+#Technology 
+Most of these tutorials will be for tech related things, this is simply beause that is an area that I run into a lot
+of problems in. It won't be all tech stuff, but a lot will.
